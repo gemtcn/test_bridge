ce2e2adcee7c374245b47b422d63651dc9ed1461
Thanh Ta <tathanh2106@gmail.com>
2021-03-10 15:34:24 +0700

    init

---
+++ js/main.js
@@ -0,0 +1,19 @@
+import $ from "jquery";
+// import bootstrap from "bootstrap/dist/js/bootstrap";
+
+$('#toggleMobileMenu').on('click', function () {
+  if ($('.br-header--main__content').hasClass('show')) {
+    $('.br-header--main__content').removeClass('show');
+    $('body').removeClass('no-scroll');
+    $(this).removeClass('show');
+  } else {
+    $('.br-header--main__content').addClass('show');
+    $('body').addClass('no-scroll');
+    $(this).addClass('show');
+  }
+})
+
+// $("a[data-tooltip='hasTooltip']").on('click', function (e) {
+//   e.preventDefault();
+//   $(this).next('.br-tooltip').toggleClass('show');
+// })
